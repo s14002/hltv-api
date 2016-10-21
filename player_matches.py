777a3b22e78cd751286250337c7528bdaa3dce5d
s14002 <s14002@std.it-college.ac.jp>
2016-10-21 13:50:45 +0900

    fix player_matches.py

--- player_matches.py
+++ player_matches.py
@@ -5,7 +5,7 @@ from python_utils import converters
 def get_parsed_page(url):
     return BeautifulSoup(requests.get(url).text, "lxml")
 
-urls = "http://www.hltv.org/?pageid=188&matchid=25001"
+urls = "http://www.hltv.org/?pageid=188&matchid=27009"
 matches = get_parsed_page(urls)
 
 """
@@ -27,9 +27,8 @@ def get_player_id(num):
         return da
 
 def get_kill(num):
-    kill = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:10%;float:left;text-align:center"})[num]
-    ki = kill.text.split()[0]
-    return ki
+    kill = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:10%;float:left;text-align:center"})[num].text.split()[0]
+    return kill
 
 def get_assist(num):
     assist = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:5%;float:left;text-align:center"})[num].text
@@ -40,7 +39,14 @@ def get_death(num):
     return death
 
 def get_kd_ratio(num):
+    """
     ratio = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:10%;float:left;text-align:center"})[num+1].text
+    return ratio
+    """
+    if (get_adr(0) == None):
+        ratio = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:10%;float:left;text-align:center"})[num+1].text
+    else:
+        ratio = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:8%;float:left;text-align:center"})[num+1].text
     return ratio
 
 def get_adr(num):
@@ -51,17 +57,32 @@ def get_adr(num):
         return None
 
 def get_rating(num):
-    rating = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:10%;float:left;text-align:center"})[num+2].text
+    #rating = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:10%;float:left;text-align:center"})
+    #return rating[num+2].text
+    if get_adr(0) == None:
+        rating = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:10%;float:left;text-align:center"})[num+2].text
+    else:
+        rating = matches.findAll("div", {"class": "covSmallHeadline", "style": "font-weight:normal;width:8%;float:left;text-align:center"})[num+2].text
     return rating
+
 m = 1
 while 11 > m:
+    if get_adr(0) == None:
+        print(get_player_id(1*m-1)) #OK
+        print(get_kill(3*m-3))
+        print(get_assist(2*m-2)) #OK
+        print(get_death(2*m-2)) #OK
+        print(get_kd_ratio(3*m-3)) #OK
+        print(get_adr(m-1))
+        print(get_rating(3*m-3))
 
-    print(get_player_id(1*m-1)) #OK
-    print(get_kill(3*m-3)) #OK
-    print(get_assist(2*m-2)) #OK
-    print(get_death(2*m-2)) #OK
-    print(get_kd_ratio(3*m-3)) #OK
-    print(get_adr(m-1))
-    print(get_rating(3*m-3)) #OK
+    else:
+        print(get_player_id(1*m-1)) #OK
+        print(get_kill(m-1)) #OK
+        print(get_assist(2*m-2)) #OK
+        print(get_death(2*m-2)) #OK
+        print(get_kd_ratio(2*m-3)) #OK
+        print(get_adr(m-1)) #OK
+        print(get_rating(2*m-3)) #OK
 
     m+=1
